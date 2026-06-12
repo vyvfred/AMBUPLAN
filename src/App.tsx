75e7a30e3ed442a42a304cfecfd08b3f67a35498
[dyad] <frederic.cave@vyv-ambulance.com>
2026-06-12 11:07:46 +0200

    [dyad] wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import NotFound from "./pages/NotFound";
 import Index from "./pages/Index";
 
@@ -57,9 +57,9 @@ const App: React.FC = () => (
       <Toaster />
       <Sonner />
       <AppProvider>
-        <BrowserRouter>
+        <HashRouter>
           <AppContent />
-        </BrowserRouter>
+        </HashRouter>
       </AppProvider>
     </TooltipProvider>
   </QueryClientProvider>
